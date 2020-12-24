b3142935706fa31d00c1201c39a05f4cbc64a31b
abdulKuddus <abdulKuddusa4@gmail.com>
2020-12-24 10:03:48 +0600

    lolo

---
+++ store/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import url
+from . import views
+
+
+urlpatterns = [
+    url(r'^$',views.catalog, name='catalog'),
+    url(r'^cart/(?P<url>\S*)?$',views.cart, name='cart'),
+    url(r'^cart/remove/$',views.removefromcart,name='remove'),
+    url(r'^cart/checkout/$',views.checkout,name='checkout'),
+    url(r'^cart/checkout/complete/$',views.completeorder, name='complete_order'),
+    url(r'^admin-login/$',views.adminLogin,name="admin_login"),
+    url(r'^admin-panel/$',views.adminDashboard,name='admin'),
+]
